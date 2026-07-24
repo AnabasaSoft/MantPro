713d5da67450f619e8faf24402fe996d688f7587
AnabasaSoft <anabasasoft@gmail.com>
2026-07-24 13:05:34 +0200

    Fix texto calendario

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ def obtener_ruta_datos():
 
 # Variable global que decide dónde se guarda TODO
 DATA_DIR = obtener_ruta_datos()
-APP_VERSION = "2.7.0"
+APP_VERSION = "2.7.1"
 REPO_OWNER = "AnabasaSoft"
 REPO_NAME = "MantPro"
 
