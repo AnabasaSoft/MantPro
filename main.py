6f3572eaafe2fa4f13a9ae1643b11f3f0cb4555b
AnabasaSoft <anabasasoft@gmail.com>
2026-09-04 07:21:58 +0200

    fix export excel y csv

--- main.py
+++ main.py
@@ -120,7 +120,7 @@ def obtener_ruta_datos():
 
 # Variable global que decide dónde se guarda TODO
 DATA_DIR = obtener_ruta_datos()
-APP_VERSION = "2.7.3"
+APP_VERSION = "2.7.4"
 REPO_OWNER = "AnabasaSoft"
 REPO_NAME = "MantPro"
 
